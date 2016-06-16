ff02518cdba01c87d25a89d0d5fd0182904f664c
Matthew Ward <iammatthewward@gmail.com>
2016-06-16 14:01:55 +0100

    adds paid status to item

--- test/unit/ItemFactory.spec.js
+++ test/unit/ItemFactory.spec.js
@@ -1,10 +1,11 @@
 describe('ItemFactory', function() {
   beforeEach(module('splitter'));
 
-  var Item;
+  var Item, paidItem;
 
   beforeEach(inject(function(ItemFactory) {
     Item = new ItemFactory({name: 'beer', price: 4.00});
+    paidItem = new ItemFactory({name: 'pizza', price: 10.00, paid: true});
   }));
 
   it('returns an item with a name', function() {
@@ -14,4 +15,13 @@ describe('ItemFactory', function() {
   it('returns an item with a price', function() {
     expect(Item.price).toEqual(4.00);
   });
+
+  it('returns an item with a default paid status of false', function() {
+    expect(Item.paid).toBe(false);
+  });
+
+  it('sets the paid status of the item if defined', function() {
+    expect(paidItem.paid).toBe(true);
+  });
+
 });
--- www/js/factories/ItemFactory.js
+++ www/js/factories/ItemFactory.js
@@ -1,10 +1,10 @@
 angular.module('splitter')
        .factory('ItemFactory', function(){
 
-  var item = function (item) {
+  var item = function(item) {
     this.name = item.name;
     this.price = item.price;
+    this.paid = item.paid === undefined ? false : item.paid;
   };
-
   return item;
 });
