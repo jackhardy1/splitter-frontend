f787344a7cab74a46ce313122e154c598398f743
Wayne Rumble <rumblewayne@gmail.com>
2016-06-16 14:56:57 +0100

    creates potentially useless item factory and consequent controller

--- test/unit/ItemService.spec.js
+++ test/unit/ItemService.spec.js
@@ -12,8 +12,8 @@ describe('ItemService', function() {
 
   it('fetches a list of items', function() {
 
-    var item1 = {name: 'beer', price: '4.00', paid: false};
-    var item2 = {name: 'chips', price: '3.00', paid: true};
+    var item1 = new ItemFactory ('beer','4.00', false);
+    var item2 = new ItemFactory ('chips', '3.00', true);
 
     var id = 1;
 
