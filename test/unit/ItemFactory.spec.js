cd02a704ecab2de30629b59ae87923cf90ff4da8
Matthew Ward <iammatthewward@gmail.com>
2016-06-16 13:41:55 +0100

    Completes ItemFactory

--- test/unit/ItemFactory.spec.js
+++ test/unit/ItemFactory.spec.js
@@ -3,11 +3,15 @@ describe('ItemFactory', function() {
 
   var Item;
 
-  beforeEach(inject(function(ItemFactory){
+  beforeEach(inject(function(ItemFactory) {
     Item = new ItemFactory({name: 'beer', price: 4.00});
   }));
 
-  it('returns an item with a name', function(){
+  it('returns an item with a name', function() {
     expect(Item.name).toEqual('beer');
+  });
+
+  it('returns an item with a price', function() {
+    expect(Item.price).toEqual(4.00);
   });
 });
