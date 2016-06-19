7a8dcd91c45d698e7f35ddc8f1bcd242cc8f9a3c
Noah Pollock <pollocknoah@gmail.com>
2016-06-19 17:30:25 +0100

    updates test files

--- www/js/services/ItemService.js
+++ www/js/services/ItemService.js
@@ -3,7 +3,7 @@ angular.module('splitter')
   var self = this;
 
   self.getAll = function(id) {
-    var url = 'http://localhost:3000/bills/' + id + '/items';
+    var url = 'http://splitter-backend.herokuapp.com/bills/' + id + '/items';
     return $http.get(url)
     .then(function(response){
       return response.data;
