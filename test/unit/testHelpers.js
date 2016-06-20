8828555d0d3a4ae566b051e9c2ebec5d6e638623
Noah Pollock <pollocknoah@gmail.com>
2016-06-20 11:31:33 +0100

    updates TestHelpers.js to get Karma tests passing

--- test/unit/testHelpers.js
+++ test/unit/testHelpers.js
@@ -1,6 +1,8 @@
 function mockRoutes($httpBackend) {
   $httpBackend.whenGET('templates/bills/new.html').respond('');
   $httpBackend.whenGET('templates/bills/show.html').respond('');
+  $httpBackend.whenGET('templates/bills/update.html').respond('');
+  $httpBackend.whenGET('templates/bills/index.html').respond('');
   $httpBackend.whenGET('templates/home.html').respond('');
 }
 
